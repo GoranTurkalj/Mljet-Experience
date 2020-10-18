d123c513ce57899035096eb9d2265dea1a8ea136
GoranTurkalj <turkalj.goran@yahoo.com>
2020-10-18 12:30:16 +0200

    video player iframe done

--- src/video.js
+++ src/video.js
@@ -1,14 +1,62 @@
-// https://www.youtube.com/embed/figIb41iTqc?autoplay=1&modestbranding=1&rel=0&loop=1
-
-//Select iframe element and when play buttons is clicked give a value to it's source attribute.
-const videoEl = document.getElementById("video");
+//Select play button
 const playBtn = document.getElementById("play-btn");
 
-playBtn.addEventListener("click", (event) => {
-  if (videoEl.getAttribute("src")) {
-    videoEl.src = "";
-  } else {
-    videoEl.src =
-      "https://www.youtube.com/embed/HWbHFiGLqnU?autoplay=1&modestbranding=1&rel=0&loop=1";
+//  This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "100%",
+    width: "100%",
+    videoId: "HWbHFiGLqnU",
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+    playerVars: {
+      modestbranding: 1,
+      autoplay: 1,
+      fs: 0,
+      iv_load_policy: 3,
+      rel: 0
+    },
+  });
+}
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+  //Here the play button is removed
+  playBtn.style.display = "none"; 
+}
+
+// The API calls this function when the player's state changes.
+function onPlayerStateChange(event) {
+  if (event.data === 0) {
+    console.log("VIDEO IS DONE.");
+    player.destroy();
+  //Here the play button shows up again
+  playBtn.style.display = "inline"; 
   }
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+//On clicking the PLAY button - the player object is created
+playBtn.addEventListener("click", (event) => { 
+  onYouTubeIframeAPIReady();
+  event.target.setAttribute("disabled", "disabled"); 
+  setTimeout(()=>{
+   // event.target.style.pointerEvents = "auto"; 
+    event.target.removeAttribute("disabled"); 
+  }, 1500); 
+ 
 });
+
